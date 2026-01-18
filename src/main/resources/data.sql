f27be9948c9ed3c16715724880033c14cef2d5ea
Emily <emily.pazmino02@epn.edu.ec>
2026-01-17 20:20:47 -0500

    Agrega data.sql con datos iniciales de productos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos(nombre, descripcion, precio, stock, categoria) VALUES
+('Laptop Asus', 'Laptop para oficina', 765.00, 5, 'Tecnolgía'),
+('Mouse', 'Mouse inalámbrico ergonómico', 25.50, 50, 'Accesorios de Cómputo'),
+('Teclado Mecánico', 'Teclado mecánico RGB', 75.99, 20, 'Accesorios de Cómputo'),
+('Audifonos Sony', 'Audifonos inalambricos', 35.00, 15, 'Audio y Sonido'),
+('Disco SSD 1TB', 'Unidad de estado sólido 1TB', 120.00, 30, 'Almacenamiento');
